916543c8443bcfd5bde56cc2d5193c37931d2211
Shantanu Jana <omni.7x@gmail.com>
2022-07-15 08:15:40 +0530

    updated bubble sort

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 void bubbleSort(int *a, int n) {
-    for(int i = 0; i < n; i++) {
+    for(int i = 1; i < n; i++) {
        
-       for(int j = 0; j < n - 1; j++) {
+       for(int j = 0; j < n - i; j++) {
            
            if(a[j] > a[j+1]) {
                swap(a[j], a[j+1]);
