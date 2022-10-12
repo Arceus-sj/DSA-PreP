c7d326a401d968241bf7b084a66aa20039f3061e
Shantanu Jana <omni.7x@gmail.com>
2022-10-12 23:28:19 +0530

    added pairs.cpp

---
+++ STL/pairs.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define int long long
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+signed main() {
+	cin.tie(NULL);
+
+
+	pair<int, string> p;
+
+	// p = make_pair(1, "Jake");
+	p = {2, "ABCD"};
+
+	cout << p.first << ", " << p.second << endl;
+
+}
+
